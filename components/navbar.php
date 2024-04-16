7be4343c44a004033ac6cbdf0f549385d49ec333
rakeputr <rakeputri27@gmail.com>
2024-04-16 23:23:25 +0700

    add home page dan style

---
+++ components/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-toggleable-md bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <i class="fa-solid fa-poo"></i>
+      kelana</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto min-vws-100">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Destination</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Reservasion</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>kelana</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <?php include ('./components/navbar.php') ?>
+
+  <section class="d-flex justify-content-center align-items-center min-vh-100 flex-column">
+    <div class="mx-auto">
+      <div class="row">
+        <div class="col-sm-2">
+          <div class="card shadow-lg">
+            <div class="card-header  text-center">
+              <h5 class="card-title">self discover escape</h5>
+              <p class="card-text">- 1 person -</p>
+            </div>
+            <div class="card-body">
+              <ul>
+                <li>1 day</li>
+                <li>include consumtion</li>
+                <li>all destination same price</li>
+                <li>with profesional tour guide</li>
+              </ul>
+            </div>
+            <div class="card-footer text-center">
+              Rp. 275.000,00
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="card shadow-lg">
+            <div class="card-header text-center">
+              <h5 class="card-title">self discover escape</h5>
+              <p class="card-text">- 1 person -</p>
+            </div>
+            <div class="card-body">
+              <ul>
+                <li>1 day</li>
+                <li>include consumtion</li>
+                <li>all destination same price</li>
+                <li>with profesional tour guide</li>
+              </ul>
+            </div>
+            <div class="card-footer text-center">
+              Rp. 275.000,00
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="card shadow-lg">
+            <div class="card-header text-center">
+              <h5 class="card-title">self discover escape</h5>
+              <p class="card-text">- 1 person -</p>
+            </div>
+            <div class="card-body">
+              <ul>
+                <li>1 day</li>
+                <li>include consumtion</li>
+                <li>all destination same price</li>
+                <li>with profesional tour guide</li>
+              </ul>
+            </div>
+            <div class="card-footer text-center">
+              Rp. 275.000,00
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="card shadow-lg">
+            <div class="card-header text-center">
+              <h5 class="card-title">self discover escape</h5>
+              <p class="card-text">- 1 person -</p>
+            </div>
+            <div class="card-body">
+              <ul>
+                <li>1 day</li>
+                <li>include consumtion</li>
+                <li>all destination same price</li>
+                <li>with profesional tour guide</li>
+              </ul>
+            </div>
+            <div class="card-footer text-center">
+              Rp. 275.000,00
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="card shadow-lg">
+            <div class="card-header text-center">
+              <h5 class="card-title">self discover escape</h5>
+              <p class="card-text">- 1 person -</p>
+            </div>
+            <div class="card-body">
+              <ul>
+                <li>1 day</li>
+                <li>include consumtion</li>
+                <li>all destination same price</li>
+                <li>with profesional tour guide</li>
+              </ul>
+            </div>
+            <div class="card-footer text-center">
+              Rp. 275.000,00
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
