9078fb01f81d202ab34f7308447769f5d4224277
Farhannivta <farhannivtasija@gmail.com>
2024-04-20 18:12:44 +0700

    include navbar in header

--- components/navbar.php
+++ components/navbar.php
@@ -1,24 +1,25 @@
-<nav style="z-index: 1000;" class="navbar navbar-expand-lg navbar-toggleable-md bg-body-tertiary position-static">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <i class="fa-solid fa-poo"></i>
-      kelana</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto min-vws-100">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="destination.php">Destination</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="booking.php">Booking</a>
-        </li>
-      </ul>
+  <nav style="z-index: 1000;" class="navbar navbar-expand-lg navbar-toggleable-md bg-body-tertiary fixed-top"
+    id="topNavbar">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <i class="fa-solid fa-poo"></i>
+        kelana</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto min-vws-100">
+          <li class="nav-item">
+            <a class="nav-link active js-scroll-trigger" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="destination.php">Destination</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="booking.php">Booking</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
